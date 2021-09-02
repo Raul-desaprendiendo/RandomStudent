363839ef47ff518c5e42cca3383d50be8f7d4648
Selene <selene.ajtd@gmail.com>
2021-09-02 11:11:51 +0200

    Cambios Selene

---
+++ SOLIDS/MailService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLIDS
+{
+    public class MailService
+    {
+        public void SendEmail(Mail mail)
+        {
+            Console.WriteLine(mail);
+        }
+
+        public virtual bool ValidateEmail(string email)
+        {
+            return email.Contains("@");
+        }
+
+
+    }
+}
